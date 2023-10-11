5a01275c89890c8570c76d6d58540ff2d7f810ef
Aneta <137305034+Aneta06@users.noreply.github.com>
2023-10-11 12:23:53 +0200

    SymmetricMatrix

---
+++ SymmetricMatrix.java
@@ -0,0 +1,21 @@
+package trialexam;
+
+public class SymmetricMatrix {
+
+    public static boolean isSymmetric(int[][] matrix) {
+        if (matrix == null) {
+            return false;
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] != matrix[j][i]) {
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
+}
+
